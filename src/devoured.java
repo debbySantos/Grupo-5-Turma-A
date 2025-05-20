f3da5ebfd6d85a546de8fd639860c0d16944a89f
debbySantos <deborassantos880@gmail.com>
2025-05-20 16:26:09 -0300

    first commit

---
+++ src/devoured.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+public class devoured {
+    static int personagemEdificuldade = 0;
+    // 1 = Fácil
+    // 2 = Médio
+    // 3 = Difícil
+
+    // NECESSÁRIO CRIAÇÃO DE STATUS PARA OS PERSONAGENS.
+
+    static String nome = "";
+    static Scanner input = new Scanner(System.in);
+
+    static int pontos = 0;
+
+    public static String registroPlayer() {
+        System.out.println("Bem-vindo(a) ao Devoured, digite seu nome: ");
+        nome = input.nextLine();
+        input.nextLine();
+        return nome;
+    }
+
+    public static int escolhaPersonagem() {
+
+        System.out.printf(
+                "Bem-vindo ao Devoured %s, escolha seu personagem para seguir na história, lembrando que está escolha afeta a dificuldade dentro do jogo \n",
+                nome);
+
+        System.out.println(
+                "1 - KIRK O Criativo Iniciante (Fácil) \n Curioso e impulsivo. Comete erros, mas aprende rápido \n Dicas visuais, erros leves, bosses previsíveis. \n");
+
+        System.out.println(
+                "2 - JUDY A Perfeccionista Intermediária (Médio) \n Técnica e exigente. Odiaria errar. \n Dicas limitadas, erros reiniciam desafios, bosses atacam duas vezes. \n");
+
+        System.out.println(
+                "3 - AVIX O Hacker Avançado (Difícil) \n Frio e experiente. Nunca pede ajuda. \n Sem dicas, erros ativam contra-ataques, bosses com habilidades únicas. \n");
+
+        System.out.println("Digite o número do personagem desejado: ");
+        while (personagemEdificuldade < 1 || personagemEdificuldade > 3) {
+            personagemEdificuldade = input.nextInt();
+            if (personagemEdificuldade < 1 || personagemEdificuldade > 3) {
+                System.out.println("Valor inválido inserido");
+            }
+        }
+
+        return personagemEdificuldade;
+    }
+
+    public static void resumoEscolhaImprimir() {
+        switch (personagemEdificuldade) {
+            case 1:
+                System.out.println("Você escolheu Kirk, o jogo estará na dificuldade fácil, boa sorte!");
+                break;
+            case 2:
+                System.out.println("Você escolheu Judy, o jogo estará na dificuldade média, boa sorte!");
+                break;
+            case 3:
+                System.out.println("Você escolheu Avix, o jogo estará na dificuldade díficil, boa sorte!");
+                break;
+
+        }
+    }
+
+    public static void iniciarMenu() {
+        int opcao = 0;
+
+        do {
+            System.out.println("\n========= MENU =========");
+            System.out.println("1. Instruções");
+            System.out.println("2. Jogar");
+            System.out.println("3. Créditos");
+            System.out.println("4. Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = input.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Função sendo implementada"); // Inserir aqui as instruções do jogo
+                    break;
+                case 2:
+                    System.out.println("Entrando no jogo...");
+                    jogar();
+                    break;
+                case 3:
+                    System.out.println("Função sendo implementada"); // Inserir aqui os créditos do jogo
+                    break;
+                case 4:
+                    System.out.println("Saindo do jogo. Até a próxima jogador!!");
+                    break;
+
+                default:
+                    break;
+            }
+        } while (opcao != 4);
+    }
+
+    public static void exibirNarrativa(String texto) {
+        System.out.println();
+        System.out.println(">>> " + texto);
+        System.out.println();
+    }
+
+    public static void jogar() {
+        registroPlayer();
+        escolhaPersonagem();
+        resumoEscolhaImprimir();
+
+        if (personagemEdificuldade == 1) {
+            exibirNarrativa(
+                    "Você desperta em um ambiente distorcido, onde comandos flutuam e erros ganham forma física. A Zona de Compilação é instável — cada passo é um teste.\n");
+            exibirNarrativa(
+                    "AVINA: “Humanos não programam mais. Eles suplicam por respostas prontas. Agora, ou vocês aprendem para sobreviver, ou ficaram presos para sempre nesse loop!”");
+
+            
+
+        }
+    }
+}
